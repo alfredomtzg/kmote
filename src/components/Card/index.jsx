751d3eb8518c1c5dc2022af0578647ebbeb91ce6
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-03-29 22:41:20 -0600

    [Update] create CardItem

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+// Style
+import './index.css'
+import he from '../../assets/he.png'
+import she from '../../assets/she.png'
+
+
+export default function Card({nombre, puesto, edad}) {
+  return (
+    <div className="cardItem">
+      <img src={he} alt="candidato"/>
+      <div className="detailContainer">
+      <h2>{nombre}</h2>
+      <h3>{puesto}</h3>
+      <h4>{edad} años</h4>
+      </div>
+    </div>
+  )
+}
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,61 +1,69 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from "react";
 // Style
 import "./index.css";
 //Image
-import logo from '../../assets/kmote_small.png'
+import logo from "../../assets/kmote_small.png";
 // Material
-import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
 // Context
 import { Context } from "../../utils/Context";
+import Card from "../Card";
 // import {useStyles} from '../../material'
-
 
 export const useStyles = makeStyles((theme) => ({
   root: {
-    '& > *': {
+    "& > *": {
       margin: theme.spacing(1),
-      width: '65ch',
+      width: "65ch",
     },
   },
 }));
 
 export default function Home() {
   const classes = useStyles();
-  const {search, setSearch, candidates, 
-    setCandidates} = useContext(Context);
+  const { search, setSearch, candidates, setCandidates } = useContext(Context);
 
   const handleChange = (event) => {
-    setValueSearch(event.target.value)
+    setSearch(event.target.value);
   };
 
   const filterCandidates = candidates.filter((candidate) => {
-    return candidate.name.toLowerCase().includes(search.toLowerCase());
+    return candidate.nombre.includes(search.toLowerCase().toUpperCase());
   });
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   console.log('submit');
-  // };
+
+  const bringData = () => {
+    var API = "https://kmote.mx/devtest/";
+    fetch(API)
+      .then((response) => response.json())
+      .then((data) => {
+        setCandidates(data?.data);
+        console.log(data?.data);
+      });
+  };
   useEffect(() => {
-    effect
-    return () => {
-      cleanup
-    }
-  }, [input])
+    bringData();
+  }, []);
   return (
     <>
-    <header >
-      <img src={logo} alt="Logo Kmote"/>
-      <form className={classes.root} >
-      <TextField 
-      autoFocus
-      value={search}
-      onChange={handleChange}
-      label="Buscar" 
-      variant="outlined" />
-      </form>
-    </header>
-      
+      <header>
+        <img src={logo} alt="Logo Kmote" />
+        <form className={classes.root}>
+          <TextField
+            autoFocus
+            value={search}
+            onChange={handleChange}
+            label="Buscar"
+            variant="outlined"
+          />
+        </form>
+      </header>
+      <main>
+        {filterCandidates.map((candidate) => {
+          return <Card key={candidate?.nombre} {...candidate} />;
+        })}
+        <button> ver mas</button>
+      </main>
     </>
-  )
+  );
 }
